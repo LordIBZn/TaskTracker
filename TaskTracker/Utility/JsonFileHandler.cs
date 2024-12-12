f490d50d9fa4ff977cebf3296311117f94661910
LordIBZn <ibzedgarovillalobos@gmail.com>
2024-12-12 15:52:52 -0800

    Se crea archivo json para guardar tareas

---
+++ TaskTracker/Utility/JsonFileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Utility
+{
+    public class JsonFileHandler<T>
+    {
+        private readonly string _fileName;
+        public JsonFileHandler(string fileName)
+        {
+            _fileName = fileName;
+
+            if (!File.Exists(_fileName))
+            {
+                File.WriteAllText(_fileName, "[]");
+            }
+        }
+
+        public List<T> Read()
+        {
+            var json = File.ReadAllText(_fileName);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+
+        public void Write(List<T> data)
+        {
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions {WriteIndented = true});
+            File.WriteAllText(_fileName, json);
+        }
+    }
+}
