27c55ab60762f85b6d00720dcd903af6ca2c988c
LordIBZn <ibzedgarovillalobos@gmail.com>
2024-12-12 13:49:56 -0800

    Se crea el modelo de tarea

---
+++ TaskTracker/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } // todo, in_progress, done
+        public DateTime Created { get; set; } 
+        public DateTime Updated { get; set; }
+    }
+}
